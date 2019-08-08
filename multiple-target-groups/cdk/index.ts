3588ce60f438fa61d0f4dc43b074d680092257cf
Pahud <pahudnet@gmail.com>
2019-08-08 23:57:18 +0800

    format

--- multiple-target-groups/cdk/index.ts
+++ multiple-target-groups/cdk/index.ts
@@ -5,107 +5,107 @@ import elbv2 = require("@aws-cdk/aws-elasticloadbalancingv2");
 
 class FargateCdkStack extends cdk.Stack {
 
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-      //new vpc
-      const vpc = new ec2.Vpc(this, 'cdk-fargate-vpc', {
-          cidr: '10.100.0.0/16',
-          maxAzs: 3
-      });
+        //new vpc
+        const vpc = new ec2.Vpc(this, 'cdk-fargate-vpc', {
+            cidr: '10.100.0.0/16',
+            maxAzs: 3
+        });
 
-      //sg for HTTP public access
-      const httpPublicSecurityGroup = new ec2.SecurityGroup(this, "HttpPublicSecurityGroup", {
-          allowAllOutbound: true,
-          securityGroupName: 'HttpPublicSecurityGroup',
-          vpc: vpc
-      });
+        //sg for HTTP public access
+        const httpPublicSecurityGroup = new ec2.SecurityGroup(this, "HttpPublicSecurityGroup", {
+            allowAllOutbound: true,
+            securityGroupName: 'HttpPublicSecurityGroup',
+            vpc: vpc
+        });
 
-      httpPublicSecurityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(80)); 
-     
-      // // Fargate Cluster
-      const fgCluster = new ecs.Cluster(this, 'fgCluster', {
-          vpc: vpc
-      });
+        httpPublicSecurityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
 
-      // ECS task definition
-      const demoTaskDef = new ecs.FargateTaskDefinition(this, 'cdk-fargate-demo-taskdef', {
-          cpu: 256,
-          memoryLimitMiB: 512,
-      })
+        // // Fargate Cluster
+        const fgCluster = new ecs.Cluster(this, 'fgCluster', {
+            vpc: vpc
+        });
 
-      const mainContainer = demoTaskDef.addContainer('main', {
-        //   image: ecs.ContainerImage.fromAsset('./nginx', {}),
-          image: ecs.ContainerImage.fromRegistry('nginx:latest'),
-          cpu: 0,
-          logging: new ecs.AwsLogDriver({
-              streamPrefix: 'fargate-dual-alb-demo'        
-          }),
-      }) 
+        // ECS task definition
+        const demoTaskDef = new ecs.FargateTaskDefinition(this, 'cdk-fargate-demo-taskdef', {
+            cpu: 256,
+            memoryLimitMiB: 512,
+        })
 
-    //   mainContainer.addLink(phpContainer, 'app')
-      mainContainer.addPortMappings({
-          containerPort: 80
-      })
+        const mainContainer = demoTaskDef.addContainer('main', {
+            //   image: ecs.ContainerImage.fromAsset('./nginx', {}),
+            image: ecs.ContainerImage.fromRegistry('nginx:latest'),
+            cpu: 0,
+            logging: new ecs.AwsLogDriver({
+                streamPrefix: 'fargate-dual-alb-demo'
+            }),
+        })
 
-      const demoService = new ecs.FargateService(this, 'demo-service', {
-          cluster: fgCluster,
-          desiredCount: 2,
-          taskDefinition: demoTaskDef,
-          securityGroup: ec2.SecurityGroup.fromSecurityGroupId(this, 'SgDemoService', vpc.vpcDefaultSecurityGroup),
-      });
+        //   mainContainer.addLink(phpContainer, 'app')
+        mainContainer.addPortMappings({
+            containerPort: 80
+        })
 
-      const healthCheckDefault = {
-        "port": 'traffic-port',
-        "path": '/',
-        "intervalSecs": 30,
-        "timeoutSeconds": 5,
-        "healthyThresholdCount": 5,
-        "unhealthyThresholdCount": 2,
-        "healthyHttpCodes": "200,301,302"
-    };      
-      
-      const externalLB = new elbv2.ApplicationLoadBalancer(this, 'external', {
-          vpc: vpc,
-          internetFacing: true,
-          securityGroup: httpPublicSecurityGroup,
-      });
+        const demoService = new ecs.FargateService(this, 'demo-service', {
+            cluster: fgCluster,
+            desiredCount: 2,
+            taskDefinition: demoTaskDef,
+            securityGroup: ec2.SecurityGroup.fromSecurityGroupId(this, 'SgDemoService', vpc.vpcDefaultSecurityGroup),
+        });
 
-      const externalListener = externalLB.addListener('PublicListener', {
-          port: 80
-      });
+        const healthCheckDefault = {
+            "port": 'traffic-port',
+            "path": '/',
+            "intervalSecs": 30,
+            "timeoutSeconds": 5,
+            "healthyThresholdCount": 5,
+            "unhealthyThresholdCount": 2,
+            "healthyHttpCodes": "200,301,302"
+        };
 
-      const internalLB = new elbv2.ApplicationLoadBalancer(this, 'internal', {
-          vpc: vpc,
-          internetFacing: false,
-          securityGroup: httpPublicSecurityGroup,
-      })
+        const externalLB = new elbv2.ApplicationLoadBalancer(this, 'external', {
+            vpc: vpc,
+            internetFacing: true,
+            securityGroup: httpPublicSecurityGroup,
+        });
 
-      const internalListener = internalLB.addListener('PrivateListener', {
-          port: 80
-      })
+        const externalListener = externalLB.addListener('PublicListener', {
+            port: 80
+        });
 
-      externalListener.addTargets('fg-echo-req', {
-          port: 80,
-          protocol: elbv2.ApplicationProtocol.HTTP,
-          healthCheck: healthCheckDefault,
-          targets: [demoService],
-          deregistrationDelay: cdk.Duration.seconds(3)
-      });
+        const internalLB = new elbv2.ApplicationLoadBalancer(this, 'internal', {
+            vpc: vpc,
+            internetFacing: false,
+            securityGroup: httpPublicSecurityGroup,
+        })
 
-      internalListener.addTargets('fg-echo-req', {
-        port: 80,
-        protocol: elbv2.ApplicationProtocol.HTTP,
-        healthCheck: healthCheckDefault,
-        targets: [demoService],
-        deregistrationDelay: cdk.Duration.seconds(3)
-    });
-   
-    new cdk.CfnOutput(this, 'ClusterARN: ', { value:  fgCluster.clusterArn });
-    new cdk.CfnOutput(this, 'ExteranlURL: ', { value:  'http://'+externalListener.loadBalancer.loadBalancerDnsName });
-    new cdk.CfnOutput(this, 'InternalURL: ', { value:  'http://'+internalListener.loadBalancer.loadBalancerDnsName });
+        const internalListener = internalLB.addListener('PrivateListener', {
+            port: 80
+        })
 
-  }
+        externalListener.addTargets('fg-echo-req', {
+            port: 80,
+            protocol: elbv2.ApplicationProtocol.HTTP,
+            healthCheck: healthCheckDefault,
+            targets: [demoService],
+            deregistrationDelay: cdk.Duration.seconds(3)
+        });
+
+        internalListener.addTargets('fg-echo-req', {
+            port: 80,
+            protocol: elbv2.ApplicationProtocol.HTTP,
+            healthCheck: healthCheckDefault,
+            targets: [demoService],
+            deregistrationDelay: cdk.Duration.seconds(3)
+        });
+
+        new cdk.CfnOutput(this, 'ClusterARN: ', { value: fgCluster.clusterArn });
+        new cdk.CfnOutput(this, 'ExteranlURL: ', { value: 'http://' + externalListener.loadBalancer.loadBalancerDnsName });
+        new cdk.CfnOutput(this, 'InternalURL: ', { value: 'http://' + internalListener.loadBalancer.loadBalancerDnsName });
+
+    }
 }
 
 exports.FargateCdkStack = FargateCdkStack;
